583eded3b36982d754f3fbd7277906d8ee106354
Burakkturk <burak1845turk@icloud.com>
2024-01-15 02:14:46 +0300

    logical

--- conditions.js
+++ conditions.js
@@ -29,14 +29,46 @@ const grade = prompt("Please enter your grade:")
 
 
 let result = ""
-if (grade<40){
+if (grade < 0){
+console.log("Grade can not be smaller than zero")
+}else if (grade< 40){
     result = "FF"
-}else if(grade <=50){
+}else if(grade <= 50){
     result ="DD"
-}else if(grade<=65){
+}else if(grade<= 65){
     result = "CC"
-}else if(grade<=89){
+}else if(grade<= 89){
     result = "BB"
-}else{
+}else if(grade <= 100){
     result = "AA"
+} else {
+    console.log("Grade can not be bigger than 100")
+}
+
+if (result != "") {
+
+console.log(`Your Score: ${result} `)}
+
+
+
+
+
+const n1 = +prompt("Num1")
+const n2 = +prompt("Num2")
+const n3 = +prompt("Num3")
+
+
+if ( n1 > n2){
+    if(n1 > n3){
+        console.log("Biggest:", n1)
+    }
+}
+
+//? kosolların logical operatorler yardımıyla birletirilmesi
+if (n1 > n2 && n1 > n3) {
+    console.log("biggest:", n1)
+}else if(n2 > n1 && n2 > n3){
+    console.log("biggest:",n2)
+}else if(n3 > n1 && n3 > n2){
+    console.log(`biggest: ${n3}`)
 }
